2c53dcbdb3522421bbba350edaa5948a0661e3af
yoonsu.kwon <kwonyoonsukwon@gmail.com>
2022-06-09 20:07:45 +0900

    git commit

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, render_template
 #import modules.inxrd_noqt, modules.outxrd_noqt
-import os
+import os, glob
 import re
+
+import numpy as np
 import pandas as pd
 from modules.calc import grain_size
 
 YEAR = 2022
 MONTH = 5
-DEFAULT_PATH = rf"\\10.138.112.112\Analysis Results\XRD\1_XRD\{YEAR}\{MONTH:02}"
+DEFAULT_PATH = rf"\\10.138.112.118\Analsis results\개인폴더\KYS\workfunction"
 app = Flask("XRD Grain Size")
 
 @app.route('/')
@@ -15,25 +17,71 @@ def root(SN_List = None):
     SN_List = os.listdir(DEFAULT_PATH)        
     return render_template('index.html', SN_List=SN_List)
 
+@app.route('/test', methods=['POST'])
+def test():
+    print(request.get_json())
+    return "hi!"
+
 @app.route('/submit', methods=['POST'])
 def login(File_List = None):
-    SN_Query = request.get_json()["SN"]    
-    File_List = os.listdir(f'{DEFAULT_PATH}\{SN_Query}')
-    File_List = [x for x in File_List if ".txt" in x or ".TXT" in x]    
+    SN_Query = request.get_json()["SN"]
+    File_List = glob.glob(f'{DEFAULT_PATH}\{SN_Query}\**', recursive=True)
+    File_List = [x for x in File_List if ".avg" in x and "Survey.avg" not in x]
+    #print(File_List)    
     Full_Dataset = {}
     Dataset = {}
+    
     for file in File_List:
-        data_path = f'{DEFAULT_PATH}\{SN_Query}\{file}'    
+        data_path = f'{file}'
+        try:
+            f = open(data_path, 'r', encoding='UTF-16')
+            data = f.readlines()
+        except UnicodeError:
+            f = open(data_path, 'r', encoding='UTF-8')
+            data = f.readlines()
+
+        x_info_idx = data.index('$SPACEAXES=1\n')
+        energy = float(data[x_info_idx+1].split(',')[0].split('=')[-1])
+        width = float(data[x_info_idx+1].split(',')[1])
+        numpoints = int(data[x_info_idx+1].split(',')[2])
+        x = 1486.68 - np.linspace(energy, energy + width*(numpoints-1), numpoints)
+        x = np.array(x[::-1])
+        y_info_idx = data.index('$DATA=*\n')
+        y = ','.join(data[y_info_idx+1:]).split(',')
+
+        if '        #empty#' in y:
+            break
+
+        for j in range(len(y)):
+            if '\n' in y[j]:
+                y[j] = y[j].strip('\n')
+            if '=' in y[j]:
+                y[j] = y[j][y[j].find('=')+1:]
+
+            y[j] = float(y[j])*4
+
+        y = np.array(y[::-1])
+        df = pd.DataFrame(zip(x, y), columns=['Energy', 'Count'])
+        print(df)
+        
+        Dataset[file] = {"Energy": df['Energy'].to_list(),
+                         "Count": df['Count'].to_list()}
+    
+        '''
         df = pd.read_csv(data_path, header=2, sep="\t")    
         df.columns = ["Angle","Count"]
         Dataset[file] = {"Angle":df["Angle"].to_list(), "Count":df["Count"].to_list(), "Peaks_Pos":[], "Peaks_Height":[]}        
-    
+        '''
     Full_Dataset[f'{SN_Query}'] = Dataset
     return Full_Dataset
+    
+
 
 @app.route('/plot', methods=['POST'])   
 def plot():
     Plot_Query = request.get_json()
+    print("print: ", Plot_Query)
+    '''
     data = Plot_Query["File"]
     sn = Plot_Query["SN"]
     data_path = f'{DEFAULT_PATH}\{sn}\{data}'    
@@ -41,7 +89,9 @@ def plot():
     df.columns = ["Angle","Count"]
     
     return {"SN":sn, "File": data, "Angle":df["Angle"].to_list(), "Count":df["Count"].to_list(), "Peaks":[]}
-
+    '''
+    return "hi"
+'''
 @app.route('/calc', methods=['POST'])   
 def calc():
     calc_query = request.get_json()
@@ -65,5 +115,5 @@ def calc():
     
     #df_result.to_csv(f'{DEFAULT_PATH}\{SN_Query})
     return "Done"
-    
-app.run(host="10.138.126.181")
+'''
+app.run(host="10.138.126.209")
--- static/script.js
+++ static/script.js
@@ -1,178 +1,35 @@
-const DEFAULT_URL = "http://10.138.126.181:5000";
+const DEFAULT_URL = "http://10.138.126.209:5000";
 const submitForm = document.querySelector("#submit-form");
 const submitInput = document.querySelector("#submit-input");
-const SNBrowser = document.querySelector("#sn-browsers");
-const fileBrowser = document.querySelector("#file-browsers");
-const fileInput = document.querySelector("#file-input");
-const modify_btn = document.querySelector("#modify-button");
-const reset_btn = document.querySelector("#reset-button");
-const plots_parent = document.querySelector("#plots-parent");
-const plots = document.querySelector("#plots");
-const STATUS = document.querySelector(".status");
-var Data;
 
-function handleSNSubmit(event) {
+function handleSubmit(event) {
   event.preventDefault();
-
-  if (submitInput.value != "") {
-    STATUS.classList.toggle("loading");
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", `${DEFAULT_URL}/submit`);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onload = function () {
-      File_List = JSON.parse(xhr.response);
-
-      data = File_List[submitInput.value];
-
-      Object.keys(data).forEach(function (key) {
-        add(key);
-        plotData(
-          submitInput.value,
-          key,
-          data[key].Angle,
-          data[key].Count,
-          data[key].Peaks_Pos,
-          data[key].Peaks_Height
-        );
-      });
-
-      Data = data;
-      STATUS.classList.toggle("loading");
-      modify_btn.classList.toggle("hidden");
-    };
-
-    let data = JSON.stringify({
-      SN: `${submitInput.value}`,
-    });
-    xhr.send(data);
-  }
-}
-
-function plotData(SN, File, Angle, Count, Peaks_Pos, Peaks_Height) {
-  colors = [];
-  sizes = [];
-  for (var i = 0; i < Angle.length; i++) {
-    colors.push("C1C1C1");
-  }
-  let PLOT = document.getElementById(`${File}_plot`);
-  let SPAN = document.getElementById(`${File}_span`);
-  SPAN.innerHTML = File;
-  var myPlot = PLOT,
-    Angle,
-    Count,
-    data = [
-      {
-        x: Angle,
-        y: Count,
-        type: "scatter",
-        mode: "markers",
-        marker: { color: colors, size: 4 },
-      },
-    ];
-  Plotly.newPlot(PLOT, data);
-  myPlot.on("plotly_click", function (data) {
-    var pn = "",
-      tn = "",
-      colors = [];
-
-    for (var i = 0; i < data.points.length; i++) {
-      pn = data.points[i].pointNumber;
-      tn = data.points[i].curveNumber;
-      colors = data.points[i].data.marker.color;
-    }
-
-    if (colors[pn] != "#FF0000") {
-      colors[pn] = "#FF0000";
-      Peaks_Pos.push(Angle[pn].toPrecision(4));
-      Peaks_Height.push(Count[pn].toPrecision(4));
-    } else {
-      colors[pn] = "#C1C1C1";
-      Peaks_Pos = arrayRemove(Peaks_Pos, Angle[pn].toPrecision(4).toString());
-      Peaks_Height = arrayRemove(
-        Peaks_Height,
-        Count[pn].toPrecision(4).toString()
-      );
-    }
-
-    var update = { marker: { color: colors, size: 4 } };
-    Plotly.restyle(`${File}_plot`, update, [tn]);
-    SPAN.innerHTML = `${File} : ${Peaks_Pos}`;
-  });
-}
-
-function add(id) {
-  const span = document.createElement("span");
-  const div = document.createElement("div");
-  span.setAttribute("id", `${id}_span`);
-  div.setAttribute("id", `${id}_plot`);
-  document.getElementById("plots").appendChild(span);
-  document.getElementById("plots").appendChild(div);
-}
-
-function remove() {
-  document.getElementById("plots").remove();
-  const div = document.createElement("div");
-  div.setAttribute("id", "plots");
-  div.setAttribute("style", "width: 1100px; height: 2000px");
-  document.getElementById("plots-parent").appendChild(div);
-  submitInput.value = "";
-}
-function handleClick(event) {
-  let current_value = document.querySelector(`#${event.path[0].id}`).value;
-  if (current_value != "") {
-    document.querySelector(`#${event.path[0].id}`).value = "";
-  }
-}
-
-function arrayRemove(arr, value) {
-  return arr.filter(function (ele) {
-    return ele != value;
-  });
-}
-
-function handleModify(event) {
-  event.preventDefault();
-
+  console.log("submitted!!")  
+  
   let xhr = new XMLHttpRequest();
-  xhr.open("POST", `${DEFAULT_URL}/calc`);
+  xhr.open("POST", `${DEFAULT_URL}/submit`);
   xhr.setRequestHeader("Accept", "application/json");
   xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onload = function () {
-    remove();
-    modify_btn.classList.toggle("hidden");
-    console.log(xhr.response);
-  };
 
-  Files = [];
-  Peaks_Pos = [];
-  Peaks_Height = [];
-
-  Object.keys(Data).forEach(function (key) {
-    Peaks_Pos.push(Data[key].Peaks_Pos);
-    Peaks_Height.push(Data[key].Peaks_Height);
-    Files.push(key);
+  SN_Select = submitInput.value
+  
+  let data = JSON.stringify({      
+    "SN":SN_Select
   });
-
-  let data = JSON.stringify({
-    SN: submitInput.value,
-    File: Files,
-    Peaks_Pos: Peaks_Pos,
-    Peaks_Height: Peaks_Height,
-  });
-
-  console.log(data);
+  
   xhr.send(data);
-  alert("Wait a Moment!");
+  
+  xhr.onload = function () {          
+    response = JSON.parse(xhr.response)
+    df = response[SN_Select]
+    console.log(df)
+    var keys = Object.keys(df)
+    keys.forEach(function(element){
+      console.log(df[element]["Count"])
+    })
+  };
 }
 
-function handleReset() {
-  remove();
-  modify_btn.classList.toggle("hidden");
-}
 
-submitForm.addEventListener("submit", handleSNSubmit);
-modify_btn.addEventListener("click", handleModify);
-reset_btn.addEventListener("click", handleReset);
-submitInput.addEventListener("click", handleClick);
+submitForm.addEventListener("submit", handleSubmit);
+//submitInput.addEventListener("click", handleClick);
